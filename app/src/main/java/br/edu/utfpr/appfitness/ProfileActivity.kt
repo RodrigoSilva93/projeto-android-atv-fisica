8d64b0f7ec8e897b6af8a650ec88f550bc34c5ce
Rodrigo Silva <rodrigo.santos.silva@outlook.com>
2025-02-19 09:50:29 -0300

    ajustes no login e adicionado email provider

--- app/src/main/java/br/edu/utfpr/appfitness/ProfileActivity.kt
+++ app/src/main/java/br/edu/utfpr/appfitness/ProfileActivity.kt
@@ -2,14 +2,22 @@ package br.edu.utfpr.appfitness
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.utfpr.appfitness.databinding.ActivityProfileBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.ktx.Firebase
 
 class ProfileActivity : AppCompatActivity() {
@@ -37,21 +45,123 @@ class ProfileActivity : AppCompatActivity() {
         }
 
         binding.btnExcluir.setOnClickListener {
-            excluirConta(user)
+            excluirConta()
         }
     }
 
-    private fun excluirConta(user: FirebaseUser?) {
-        user?.delete()?.addOnCompleteListener { task ->
+    private fun excluirConta() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user == null) {
+            Toast.makeText(this, "Usuário não autenticado.", Toast.LENGTH_SHORT).show()
+            return
+        }
+        val userId = user.uid
+
+        FirebaseFirestore.getInstance().collection("Pessoa")
+            .document(userId).get()
+            .addOnSuccessListener { document ->
+                if (document.exists() && document.contains("loginMethod")) {
+                    val loginMethod = document.getString("loginMethod")
+
+                    when (loginMethod) {
+                        "email_password" -> solicitarSenhaParaExcluirConta()
+                        "google" -> excluirContaGoogle()
+                        else -> Toast.makeText(this, "Método de login desconhecido.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                else Toast.makeText(this, "Erro ao buscar dados do usuário.", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun excluirContaGoogle() {
+        val user = FirebaseAuth.getInstance().currentUser
+
+        user?.getIdToken(true)?.addOnCompleteListener { task ->
             if (task.isSuccessful) {
-                Toast.makeText(this, "Conta excluída.", Toast.LENGTH_SHORT).show()
-                startActivity(Intent(this, LoginActivity::class.java))
-                finish()
-            } else {
-                Toast.makeText(this, "Erro ao excluir conta.", Toast.LENGTH_SHORT).show()
+                val idToken = task.result?.token
+                Log.d("PROFILE", "idToken: $idToken")
+                if (idToken != null) {
+                    val credential = GoogleAuthProvider.getCredential(idToken, null)
+                    user.reauthenticate(credential).addOnCompleteListener { authTask ->
+                        if (authTask.isSuccessful) {
+                            user.delete().addOnCompleteListener { deleteTask ->
+                                if (deleteTask.isSuccessful) {
+                                    Toast.makeText(
+                                        this,
+                                        "Conta excluída com sucesso.",
+                                        Toast.LENGTH_SHORT
+                                    ).show()
+                                    startActivity(Intent(this, LoginActivity::class.java))
+                                    finish()
+                                } else {
+                                    Toast.makeText(this, "Erro ao excluir conta.", Toast.LENGTH_SHORT).show()
+                                }
+                            }
+                        } else {
+                            Toast.makeText(this, "Erro ao reautenticar.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                } else {
+                    Log.w("PROFILE", "Erro ao recuperar token", task.exception)
+                    Toast.makeText(this, "Erro ao recuperar token.", Toast.LENGTH_SHORT).show()
+                }
             }
         }
     }
+
+    private fun solicitarSenhaParaExcluirConta() {
+        val user = FirebaseAuth.getInstance().currentUser
+        if (user?.email == null) return
+
+        val input = EditText(this)
+        input.inputType = InputType.TYPE_TEXT_VARIATION_PASSWORD or InputType.TYPE_CLASS_TEXT
+
+        AlertDialog.Builder(this)
+            .setTitle("Confirme sua Senha")
+            .setMessage("Digite sua senha para excluir a conta.")
+            .setView(input)
+            .setPositiveButton("Confirmar") { _, _ ->
+                val senha = input.text.toString()
+                if (senha.isNotEmpty()) {
+                    reautenticarEExcluir(user.email!!, senha)
+                } else {
+                    Toast.makeText(this, "Senha não pode estar vazia.", Toast.LENGTH_SHORT).show()
+                }
+            }
+            .setNegativeButton("Cancelar", null)
+            .show()
+    }
+
+    private fun reautenticarEExcluir(email: String, senha: String) {
+        val user = FirebaseAuth.getInstance().currentUser
+        val credential = EmailAuthProvider.getCredential(email, senha)
+
+        user?.reauthenticate(credential)?.addOnCompleteListener { authTask ->
+            if (authTask.isSuccessful) {
+                val userId = user.uid
+                FirebaseFirestore.getInstance().collection("Pessoa").document(userId)
+                    .delete()
+                    .addOnSuccessListener {
+                        user.delete().addOnCompleteListener { deleteTask ->
+                            if (deleteTask.isSuccessful) {
+                                Toast.makeText(this, "Conta excluída com sucesso.", Toast.LENGTH_SHORT).show()
+                                startActivity(Intent(this, LoginActivity::class.java))
+                                finish()
+                            } else {
+                                Toast.makeText(this, "Erro ao excluir conta.", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(this, "Erro ao excluir conta.", Toast.LENGTH_SHORT).show()
+                    }
+
+            } else {
+                Toast.makeText(this, "Senha incorreta.", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
 
 
 }
--- app/src/main/java/br/edu/utfpr/appfitness/RegisterActivity.kt
+++ app/src/main/java/br/edu/utfpr/appfitness/RegisterActivity.kt
@@ -2,12 +2,15 @@ package br.edu.utfpr.appfitness
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import br.edu.utfpr.appfitness.databinding.ActivityRegisterBinding
+import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
@@ -15,8 +18,12 @@ import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
-    private lateinit var binding: ActivityRegisterBinding
     private val db = Firebase.firestore
+
+    private lateinit var btnRegister: Button
+    private lateinit var nomeEditText: TextInputEditText
+    private lateinit var emailEditText: TextInputEditText
+    private lateinit var senhaEditText: TextInputEditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +31,16 @@ class RegisterActivity : AppCompatActivity() {
 
         auth = Firebase.auth
 
-        binding.btnRegister.setOnClickListener {
-            val nome = binding.textNome.text.toString().trim()
-            val email = binding.textEmail.text.toString().trim()
-            val password = binding.textSenha.text.toString().trim()
+        btnRegister = findViewById(R.id.btnRegister)
+        nomeEditText = findViewById(R.id.textNome)
+        emailEditText = findViewById(R.id.textEmail)
+        senhaEditText = findViewById(R.id.textSenha)
+
+
+        btnRegister.setOnClickListener {
+            val nome = nomeEditText.text.toString().trim()
+            val email = emailEditText.text.toString().trim()
+            val password = senhaEditText.text.toString().trim()
 
             if (nome.isEmpty() || email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "Preencha todos os campos.", Toast.LENGTH_SHORT).show()
@@ -41,7 +54,8 @@ class RegisterActivity : AppCompatActivity() {
                         val usuario = hashMapOf(
                             "uid" to (user?.uid ?: ""),
                             "nome" to nome,
-                            "email" to email
+                            "email" to email,
+                            "loginMethod" to "email_password"
                         )
 
                         db.collection("Pessoa").document(user!!.uid)
@@ -51,16 +65,16 @@ class RegisterActivity : AppCompatActivity() {
                                 startActivity(Intent(this, LoginActivity::class.java))
                                 finish()
                             }
-                            .addOnFailureListener { 
+                            .addOnFailureListener { e ->
+                                Log.e("REGISTER", "Erro ao salvar usuário", e)
                                 Toast.makeText(this, "Erro ao criar usuário!", Toast.LENGTH_SHORT).show()
                             }
                     }
-                    else Toast.makeText(this, "Erro no cadastro!", Toast.LENGTH_SHORT).show()
-
+                    else {
+                        Log.e("REGISTER", "Erro no cadastro", task.exception)
+                        Toast.makeText(this, "Erro no cadastro!", Toast.LENGTH_SHORT).show()
+                    }
                 }
-
-
-
         }
     }
 }
