c62b414c97b7259391be0e6ab9ea97584d85f276
Rodrigo Silva <rodrigo.santos.silva@outlook.com>
2025-02-22 21:29:24 -0300

    Melhorias no layout

--- app/src/main/java/br/edu/utfpr/appfitness/MainActivity.kt
+++ app/src/main/java/br/edu/utfpr/appfitness/MainActivity.kt
@@ -2,29 +2,32 @@ package br.edu.utfpr.appfitness
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import br.edu.utfpr.appfitness.fragment.FeedFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.hide()
+
         val user = Firebase.auth.currentUser
-        if (user != null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, FeedFragment())
-                .commit()
-        } else {
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
+        if (user == null) {
+            // Se não estiver autenticado, redireciona para a tela de login
+            startActivity(Intent(this, LoginActivity::class.java))
             finish()
+            return
         }
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
     }
 }
